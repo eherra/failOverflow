c3b3b7de9424ca7828bd162f5ee89e87c1f9b78a
Elias Herranen <elias.herranen@gmail.com>
2023-01-31 20:16:48 +0200

    Added react import

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Grommet } from 'grommet';
 import { Spinner } from 'grommet';
+import React from 'react';
 
 const customTheme = {
   global: {
