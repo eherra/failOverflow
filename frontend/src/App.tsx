ee22b47707b2bb2b66efde03a6fb522c69c0701a
Elias Herranen <elias.herranen@gmail.com>
2023-01-31 18:08:10 +0200

    Added prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import React, { Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Spinner } from 'grommet';
 
-import HomePage from "./pages/home/HomePage";
-import ProfilePage from "./pages/profile/ProfilePage";
-import LoginPage from "./pages/auth/pages/login/LoginPage";
-import LandingPage from "./pages/landing/LandingPage";
-import RegisterPage from "./pages/auth/pages/register/RegisterPage";
-import ProfileEditPage from "./pages/profile/pages/ProfileEditPage";
+import HomePage from './pages/home/HomePage';
+import ProfilePage from './pages/profile/ProfilePage';
+import LoginPage from './pages/auth/pages/login/LoginPage';
+import LandingPage from './pages/landing/LandingPage';
+import RegisterPage from './pages/auth/pages/register/RegisterPage';
+import ProfileEditPage from './pages/profile/pages/ProfileEditPage';
 
-const ProfileOverviewPage = React.lazy(() => import('./pages/profile/pages/ProfileOverviewPage'))
-const FailuresPage = React.lazy(() => import('./pages/failures/FailuresPage'))
+const ProfileOverviewPage = React.lazy(
+  () => import('./pages/profile/pages/ProfileOverviewPage'),
+);
+const FailuresPage = React.lazy(() => import('./pages/failures/FailuresPage'));
 
 const App = () => {
   return (
@@ -23,16 +25,22 @@ const App = () => {
         <Route path="register" element={<RegisterPage />} />
         <Route path="profile" element={<ProfilePage />} />
         <Route path="profile/edit" element={<ProfileEditPage />} />
-        <Route path="profile/overview" element={
-          <Suspense fallback={<Spinner size='large' alignSelf='center' />}>
-            <ProfileOverviewPage />
-          </Suspense>
-        } />
-        <Route path="failures" element={
-          <Suspense fallback={<Spinner size='large' alignSelf='center' />}>
-            <FailuresPage />
-          </Suspense>
-        } />
+        <Route
+          path="profile/overview"
+          element={
+            <Suspense fallback={<Spinner size="large" alignSelf="center" />}>
+              <ProfileOverviewPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="failures"
+          element={
+            <Suspense fallback={<Spinner size="large" alignSelf="center" />}>
+              <FailuresPage />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
