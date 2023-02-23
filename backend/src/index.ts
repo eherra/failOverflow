63c9d5e74f024c2462fd64e62a6a04e128127b42
Elias Herranen <elias.herranen@gmail.com>
2023-02-23 20:59:37 +0200

    Modified configs to support MongoDB atlas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import { MONGODB_URI } from "./utils/config";
+import { MONGODB_URI_ATLAS } from "./utils/config";
 import { PORT } from "./utils/config";
 import app from "./app";
 import logger from "./utils/logger";
@@ -6,9 +6,9 @@ import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
 
-logger.info("connecting to", MONGODB_URI);
+logger.info("connecting to MongoDB");
 mongoose
-  .connect(MONGODB_URI as string)
+  .connect(MONGODB_URI_ATLAS as string) // if using local docker, change "MONGODB_URI_ATLAS" -> "MONGODB_URI_DOCKER"
   .then(() => {
     logger.info("connected to MongoDB");
   })
